73de94fac3da5ebfb33d623cfc2deed38f7d2c5f
JesusReyRod <Jesusreynaga96@gmail.com>
2017-06-09 02:04:23 -0500

    Menu contactos
    solo faltan las notificaciones

--- OjoMetropolitano/app/src/main/java/com/example/inzod/ojometropolitano/Contactos.java
+++ OjoMetropolitano/app/src/main/java/com/example/inzod/ojometropolitano/Contactos.java
@@ -1,12 +1,16 @@
 package com.example.inzod.ojometropolitano;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -22,20 +26,49 @@ public class Contactos extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contactos);
+
+        contactos = new ArrayList<String>();
+        adaptador1 = new ArrayAdapter<String>(this,R.layout.cutom,contactos);
+        myList = (ListView)findViewById(R.id.myListaContactos);
+        myList.setAdapter(adaptador1);
+
         myEditTXT = (EditText)findViewById(R.id.addContacto);
         btn = (Button)findViewById(R.id.btnAddCont);
-        contactos = new ArrayList<String>();
-        myList = (ListView)findViewById(R.id.myListaContactos);
 
-        adaptador1 = new ArrayAdapter<String>(getApplicationContext(), R.layout.cutom,contactos);
-        myList.setAdapter(adaptador1);
+        myList.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(Contactos.this);
+                builder.setTitle("Elije una Opción");
+                builder.setItems(new String[]{"Compartir ubicación", "Solicitar ubicación"}, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        switch (which){
+                            case 0:
+                                Toast.makeText(Contactos.this,"Selec. Compartir ubicación", Toast.LENGTH_LONG).show();
+                                break;
+                            case 1:
+                                Toast.makeText(Contactos.this,"Selec. Solicitar ubicación", Toast.LENGTH_LONG).show();
+                                break;
+                        }
+                    }
+                });
+                builder.show();
+                return false;
+            }
+        });
+
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 contactos.add(myEditTXT.getText().toString());
                 adaptador1.notifyDataSetChanged();
+                myEditTXT.setText("");
             }
         });
+
     }
+
 }
